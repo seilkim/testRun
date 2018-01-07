9504e6b7c5111ec74be35439bca92aedbcc8cb1e
seilkim <seilkim87@gmail.com>
2018-01-07 19:05:56 +0900

    게시판 완성

--- Test01/src/main/java/first/sample/controller/SampleController.java
+++ Test01/src/main/java/first/sample/controller/SampleController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import first.common.common.CommandMap;
 import first.sample.service.SampleService;
 
 @Controller
@@ -19,17 +20,68 @@ public class SampleController {
 	@Resource(name="sampleService")
 	private SampleService sampleService;
 	
-	
-	//commit;
-	@RequestMapping(value="/openSampleBoardList.do")
-    public ModelAndView openSampleBoardList(Map<String,Object> commandMap) throws Exception{
+	@RequestMapping(value="/openBoardList.do")
+    public ModelAndView openBoardList(CommandMap commandMap) throws Exception{
     	ModelAndView mv = new ModelAndView("/sample/boardList");
     	
-    	System.out.println("aaaa");
-    	
-    	List<Map<String,Object>> list = sampleService.selectBoardList(commandMap);
+    	List<Map<String,Object>> list = sampleService.selectBoardList(commandMap.getMap());
     	mv.addObject("list", list);
     	
     	return mv;
     }
+	
+	@RequestMapping(value="/openBoardWrite.do")
+	public ModelAndView openBoardWrite(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("/sample/boardWrite");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/insertBoard.do")
+	public ModelAndView insertBoard(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("redirect:/openBoardList.do");
+		
+		sampleService.insertBoard(commandMap.getMap());
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/openBoardDetail.do")
+	public ModelAndView openBoardDetail(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("/sample/boardDetail");
+		
+		Map<String,Object> map = sampleService.selectBoardDetail(commandMap.getMap());
+		mv.addObject("map", map);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/openBoardUpdate.do")
+	public ModelAndView openBoardUpdate(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("/sample/boardUpdate");
+		
+		Map<String,Object> map = sampleService.selectBoardDetail(commandMap.getMap());
+		mv.addObject("map", map);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/updateBoard.do")
+	public ModelAndView updateBoard(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("redirect:/openBoardDetail.do");
+		
+		sampleService.updateBoard(commandMap.getMap());
+		
+		mv.addObject("IDX", commandMap.get("IDX"));
+		return mv;
+	}
+	
+	@RequestMapping(value="/deleteBoard.do")
+	public ModelAndView deleteBoard(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("redirect:/openBoardList.do");
+		
+		sampleService.deleteBoard(commandMap.getMap());
+		
+		return mv;
+	}
 }
--- Test01/src/main/java/first/sample/dao/SampleDAO.java
+++ Test01/src/main/java/first/sample/dao/SampleDAO.java
@@ -12,9 +12,28 @@ public class SampleDAO extends AbstractDAO{
 
 	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception{
-		
-		System.out.println("DAO :: selectBoardList");
 		return (List<Map<String, Object>>)selectList("sample.selectBoardList", map);
+	}
+
+	public void insertBoard(Map<String, Object> map) throws Exception{
+		insert("sample.insertBoard", map);
+	}
+
+	public void updateHitCnt(Map<String, Object> map) throws Exception{
+		update("sample.updateHitCnt", map);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> selectBoardDetail(Map<String, Object> map) throws Exception{
+		return (Map<String, Object>) selectOne("sample.selectBoardDetail", map);
+	}
+
+	public void updateBoard(Map<String, Object> map) throws Exception{
+		update("sample.updateBoard", map);
+	}
+
+	public void deleteBoard(Map<String, Object> map) throws Exception{
+		update("sample.deleteBoard", map);
 	}
 
 }
